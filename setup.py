acc151330dfdeb72d4ea6d00118415635aa2b908
sgoggins <s@goggins.com>
2023-08-12 12:20:43 -0500

    trying to see if the latest pip really gets everything right.

--- setup.py
+++ setup.py
@@ -35,55 +35,105 @@ setup(
     install_requires=[
         "wheel",
         "sendgrid",
-        "alembic==1.8.1", # 1.8.1
-        "coloredlogs==15.0", # 15.0.1
-        "Beaker==1.11.0", # 1.11.0
-        "SQLAlchemy==2.0.17", # 1.4.40
-        "itsdangerous==2.1.2", # 2.1.2
-        'Jinja2==3.1.2',
-        "Flask==2.3.2", # 2.2.2
-        "Flask-Cors==3.0.10",
-        "Flask-Login==0.6.2",
-        "Flask-WTF==1.0.0",
-        "pandas==1.5.3", # 1.4.3
-        "numpy==1.24.2", # 1.23.2
-        "requests==2.28.2", # 2.28.1
-        "psycopg2-binary==2.9.6", #2.9.3 what is pscopg-binary 3.0.16
-        "click <=9.0, >=8.1.3", # 8.1.3
-        "psutil==5.8.0", # 5.9.1
-        "gunicorn==20.1.0", # 20.1.0
-        "six==1.15.0", # 1.16.0
-        "bokeh==2.0.2", # 2.4.3
-        "selenium==3.141.0",# 4.4.3
-        "dask>=2021.6.2", # 2022.8.1
-        "cloudpickle >= 0.2.2", # 2.1.0
-        "fsspec >= 0.6.0", # 2022.7.1
-        "toolz >= 0.8.2", # 0.12.0
-        "partd >= 0.3.10", # 1.3.0
-        "distributed >= 2021.03.0", # 2022.8.1
-        "nltk==3.6.6", # 3.7
-        "h5py~=3.8.0", # 3.7
-        "scipy==1.10.1", # 1.9.0
-        "blinker>=1.6.2", # 1.5
-        "protobuf<3.22", # 4.21.5
-        "slack==0.0.2", # 0.0.2
-        "boto3==1.17.57", # 1.24.56
-        "toml", # 0.10.2
-        "mistune<3.0, >=2.0", # 2.0.4
-        "pyYaml", # 6.0
-        "redis==4.3.3", # 4.3.4
-        "XlsxWriter==1.3.7", # 3.0.3
-        "celery==5.2.7", # 5.2.7
-        "httpx==0.23.0", # 0.23.0
-        "eventlet==0.33.3", 
-        "flower==1.2.0",
-        "tornado==6.1", # added because it sometimes errors when tornado is not 6.1 even though nothing we install depends on it
-        "pylint==2.15.5",
-        "dnspython==2.2.1",
-        "Werkzeug>=2.3.6",
-        "pylint==2.15.5",
-        "cmake>=3.26.4"
+        "alembic",
+        "coloredlogs",
+        "Beaker",
+        "SQLAlchemy",
+        "itsdangerous",
+        "Jinja2",
+        "Flask",
+        "Flask-Cors",
+        "Flask-Login",
+        "Flask-WTF",
+        "pandas",
+        "numpy",
+        "requests",
+        "psycopg2-binary",
+        "click",
+        "psutil",
+        "gunicorn",
+        "six",
+        "bokeh",
+        "selenium",
+        "dask",
+        "cloudpickle",
+        "fsspec",
+        "toolz",
+        "partd",
+        "distributed",
+        "nltk",
+        "h5py",
+        "scipy",
+        "blinker",
+        "protobuf",
+        "slack",
+        "boto3",
+        "toml", 
+        "mistune",
+        "pyYaml", 
+        "redis",
+        "XlsxWriter",
+        "celery",
+        "httpx",
+        "eventlet",
+        "flower",
+        "tornado",
+        "pylint",
+        "dnspython",
+        "Werkzeug",
+        "pylint",
+        "cmake"
     ],
+        # "wheel",
+        # "sendgrid",
+        # "alembic==1.8.1", # 1.8.1
+        # "coloredlogs==15.0", # 15.0.1
+        # "Beaker==1.11.0", # 1.11.0
+        # "SQLAlchemy==2.0.17", # 1.4.40
+        # "itsdangerous==2.1.2", # 2.1.2
+        # 'Jinja2==3.1.2',
+        # "Flask==2.3.2", # 2.2.2
+        # "Flask-Cors==3.0.10",
+        # "Flask-Login==0.6.2",
+        # "Flask-WTF==1.0.0",
+        # "pandas==1.5.3", # 1.4.3
+        # "numpy==1.24.2", # 1.23.2
+        # "requests==2.28.2", # 2.28.1
+        # "psycopg2-binary==2.9.6", #2.9.3 what is pscopg-binary 3.0.16
+        # "click <=9.0, >=8.1.3", # 8.1.3
+        # "psutil==5.8.0", # 5.9.1
+        # "gunicorn==20.1.0", # 20.1.0
+        # "six==1.15.0", # 1.16.0
+        # "bokeh==2.0.2", # 2.4.3
+        # "selenium==3.141.0",# 4.4.3
+        # "dask>=2021.6.2", # 2022.8.1
+        # "cloudpickle >= 0.2.2", # 2.1.0
+        # "fsspec >= 0.6.0", # 2022.7.1
+        # "toolz >= 0.8.2", # 0.12.0
+        # "partd >= 0.3.10", # 1.3.0
+        # "distributed >= 2021.03.0", # 2022.8.1
+        # "nltk==3.6.6", # 3.7
+        # "h5py~=3.8.0", # 3.7
+        # "scipy==1.10.1", # 1.9.0
+        # "blinker>=1.6.2", # 1.5
+        # "protobuf<3.22", # 4.21.5
+        # "slack==0.0.2", # 0.0.2
+        # "boto3==1.17.57", # 1.24.56
+        # "toml", # 0.10.2
+        # "mistune<3.0, >=2.0", # 2.0.4
+        # "pyYaml", # 6.0
+        # "redis==4.3.3", # 4.3.4
+        # "XlsxWriter==1.3.7", # 3.0.3
+        # "celery==5.2.7", # 5.2.7
+        # "httpx==0.23.0", # 0.23.0
+        # "eventlet==0.33.3", 
+        # "flower==1.2.0",
+        # "tornado==6.1", # added because it sometimes errors when tornado is not 6.1 even though nothing we install depends on it
+        # "pylint==2.15.5",
+        # "dnspython==2.2.1",
+        # "Werkzeug>=2.3.6",
+        # "pylint==2.15.5",
+        # "cmake>=3.26.4"    
     extras_require={
         "dev": [
             "tox==4.6.0", # 3.25.1
